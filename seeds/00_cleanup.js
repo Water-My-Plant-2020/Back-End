302e831d9a1e441d9104db2be83a565fc58ed1dd
Dodger-42 <kevin.bing.barber@gmail.com>
2020-11-12 16:26:27 -0800

    plants.js, user, user-model, user-router, seeds.. plants, user, species, middleware setup, .env

--- seeds/00_cleanup.js
+++ seeds/00_cleanup.js
@@ -1,10 +1,7 @@
-exports.seed = async function(knex) {
-    await knex('food').truncate()
-    await knex('food').insert([
-          {name: 'Chicken Tacos', type: 'entree'},
-          {name: 'Black Beans', type: 'side'},
-          {name: 'Mex Rice', type: 'side'},
-          {name: 'Guac and Chips', type: 'side'},
-          {name: 'Cadillac Margarita', type: 'drink'}
-        ]);
-  };
+const cleaner = require('knex-cleaner');
+
+exports.seed = function(knex) {
+  return cleaner.clean(knex, {
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock'], // don't empty migration tables
+  });
+};
--- seeds/02_user.js
+++ seeds/02_user.js
@@ -1,10 +1,20 @@
-exports.seed = async function(knex) {
-    await knex('user').truncate()
-    await knex('user').insert([
-          {name: '', type: ''},
-          {name: '', type: ''},
-          {name: '', type: 'e'},
-          {name: '', type: ''},
-          {name: '', type: ''}
-        ]);
-  };
+const bcrypt = require("bcrypt");
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {id: 1, username: 'Johndoe', password: bcrypt.hashSync('abc1234', 14)},
+        {id: 2, username: 'Janedoe', password: bcrypt.hashSync('abc12345', 14)},
+        {id: 2, username: 'Bobdoe', password: bcrypt.hashSync('abc123456', 14)},
+        {id: 2, username: 'Marrisadoe', password: bcrypt.hashSync('abc1234567', 14)},
+        {id: 2, username: 'Joshdoe', password: bcrypt.hashSync('bc12345', 14)},
+        {id: 2, username: 'Josedoe', password: bcrypt.hashSync('c12345', 14)},
+        {id: 2, username: 'Kevindoe', password: bcrypt.hashSync('bc123456', 14)},
+        {id: 2, username: 'Cullendoe', password: bcrypt.hashSync('c123456', 14)},
+
+      ]);
+    });
+};
