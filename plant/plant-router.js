366fc716eb10619e3f13ca79bfc6ee9ad9ea4937
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-11-17 19:52:16 -0600

    removed duplicated endpoints

--- plant/plant-router.js
+++ plant/plant-router.js
@@ -71,26 +71,4 @@ router.delete('/:id', (req,res) => {
       });
 })
 
-router.post('/', async (req,res, next) => {
-     try {
-		const data = await plants.addPlant(req.body)
-		res.status(201).json(data)
-	} catch(err) {
-		next(err)
-	}
-})
-
-router.delete('/:id', (req,res) => {
-    const {id} = req.params;
-    plants.removePlant(id).then(deleted => {
-        if(deleted){
-            res.json({removed: deleted})
-        }else{
-            res.status(404).json({message: 'no plant matching that ID exists'})
-        }
-    }).catch(err => {
-        res.status(500).json({ message: 'an error has occurred' });
-      });
-})
-
 module.exports = router;
